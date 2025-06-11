4be17a19e92193392a872a71ecc4866387e2a275
Mirely Bressan Damian <71570381+MirelyBressan@users.noreply.github.com>
2025-06-11 22:51:28 +0000

    .

--- Atividades-Java/aula-07/src/App.java
+++ Atividades-Java/aula-07/src/App.java
@@ -1,5 +1,26 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        ArrayList<String> aulas = new ArrayList<>();
+
+        String aula1 = "Programação";
+        String aula2 = "Banco de Dados";
+        String aula3 = "Qualidade de Software";
+
+        aulas.add(aula1);
+        aulas.add(aula2);
+        aulas.add(aula3);
+       //aulas.remove(1);
+
+       for (String aula : aulas) {
+            System.out.println("Aula: " + aula);
+        }
+
+        System.out.println("Aula: " + aulas.get(1));
+
+        Collections.sort(aulas); // Ordena as aulas
+        Collections.reverse(aulas); // Inverte a ordem
     }
 }
